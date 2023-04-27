9db4370c1a7611d027a24ec22f6d96e356093a9a
jrs0ul <jrs0ul@gmail.com>
2023-04-27 23:28:41 +0300

    well the bunnies are fleeing, kind of

--- src/data/npc_data.asm
+++ src/data/npc_data.asm
@@ -1,4 +1,4 @@
-;first sprite index, rows, collision offset Y, type(1-predator, 0-neutral), max_hp
+;first sprite index, rows, collision offset Y, type(1-predator, 0-timid, 2 - villager), max_hp
 npc_data:
     .byte 80, 3, 17, 1, 6, 0, 0 ,0      ;werewolf
     .byte 10, 2, 9,  0, 2, 0, 0, 0      ;bunny
