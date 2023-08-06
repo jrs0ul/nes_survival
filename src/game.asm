5d6d92ce949acc6323302691cb3adef09a89dad4
jrs0ul <jrs0ul@gmail.com>
2023-08-06 18:11:09 +0300

    moved intro code and gameover/title routines to bank 5

--- src/game.asm
+++ src/game.asm
@@ -1247,24 +1247,7 @@ vblankwait2:      ; Second wait for vblank, PPU is ready after this
     ldy #5
     bankswitch
 
-    lda #<title_palette
-    sta pointer
-    lda #>title_palette
-    sta pointer + 1
-    lda #32
-    sta Temp
-    jsr LoadPalette
-
-
-    lda #<title
-    sta pointer
-    lda #>title
-    sta pointer+1
-
-    lda #$20
-    sta NametableAddress
-
-    jsr LoadNametable
+    jsr LoadTitleData ; from bank 5
 
     lda #%10010000   ; enable NMI, sprites from Pattern Table 0
     sta PPUCTRL
@@ -1273,8 +1256,6 @@ vblankwait2:      ; Second wait for vblank, PPU is ready after this
     lda #%00011110   ; enable sprites
     sta $2001
 
-    lda #STATE_TITLE
-    sta GameState
 
     lda #INPUT_DELAY
     sta InputUpdateDelay
@@ -1370,7 +1351,6 @@ doSoundUpdate:
     jsr famistudio_update
 
     ldy oldbank
-
     bankswitch
 
     lda GameState
@@ -1399,11 +1379,7 @@ checkIntro:
     dec IntroDelay
     bne runrandom
 
-    ldx IntroSceneIdx
-    lda intro_scenes_delay, x
-    sta IntroDelay
-    jsr IntroLogics
-    jsr UpdateIntroSprites
+    jsr doIntro
     jmp runrandom
 
 hide_sprites:
@@ -1591,6 +1567,23 @@ noBankSwitch:
 .include "LoadOutsideMap.asm"
 .include "random.asm"
 .include "menu.asm"
+.include "IntroCode.asm"
+
+;---------------------------------
+doIntro:
+    ldx IntroSceneIdx
+    lda intro_scenes_delay, x
+    sta IntroDelay
+    ldy current_bank
+    sty oldbank
+    ldy #5
+    bankswitch
+    jsr IntroLogics
+    jsr UpdateIntroSprites
+    ldy oldbank
+    bankswitch
+    rts
+
 
 
 ;--------------------------------------------
@@ -1638,166 +1631,6 @@ LoadBackgroundsIfNeeded:
 @exit:
     rts
 
-;---------------------------------
-UpdateIntroSprites:
-
-
-    lda #<IntroSprite1X
-    sta IntroSpriteCoordPtr
-    lda #>IntroSprite1X
-    sta IntroSpriteCoordPtr + 1
-
-
-    ldx IntroSceneIdx
-    cpx #INTRO_SCENE_MAX
-    bcs @done
-
-    ldy #0
-    sty TempSpriteCount
-    sty IntroMetaspriteIndex
-
-    lda intro_meta_sprite_count, x
-    sta IntroMetaspriteCount
-
-
-    txa
-    asl
-    tax
-
-@metaspriteloop:
-    lda intro_sprite_count, x
-    sta IntroSpriteCount
-
-    lda IntroSpriteAnimFrame
-    bne @secondFrame
-
-@firstFrame:
-    lda intro_sprites_low, x
-    sta IntroSpritePtr
-    lda intro_sprites_high, x
-    sta IntroSpritePtr + 1
-    jmp @cont
-@secondFrame:
-    lda intro_sprites_2_low, x
-    sta IntroSpritePtr
-    lda intro_sprites_2_high, x
-    sta IntroSpritePtr + 1
-    clc
-    adc IntroSpritePtr
-    beq @firstFrame
-
-@cont:
-    tya
-    sta TempPointY2 ;backup y for writing
-    sec
-    sbc TempSpriteCount
-    sta TempPointY ; backup y for reading
-    tay
-
-
-@updateLoop:
-
-    jsr DoIntroSpriteUpdate
-
-    cpy IntroSpriteCount
-    bcc @updateLoop
-
-    lda TempSpriteCount
-    clc
-    adc IntroSpriteCount
-    sta TempSpriteCount
-
-    inc IntroSpriteCoordPtr ;increment pointer to point to next metasprite coordinates
-    inc IntroSpriteCoordPtr
-
-    inc IntroMetaspriteIndex
-    inx
-    lda IntroMetaspriteIndex
-    cmp IntroMetaspriteCount
-    bcc @metaspriteloop
-
-
-    lda TempSpriteCount
-    lsr
-    lsr
-    sta TempSpriteCount
-
-    ldy TempPointY2
-    jsr HideIntroSprites
-
-@done:
-    rts
-;---------------------------------
-DoIntroSpriteUpdate:
-
-    ldy #1
-    lda (IntroSpriteCoordPtr), y ; y coord
-    ldy TempPointY
-    clc
-    adc (IntroSpritePtr), y
-
-    ldy TempPointY2
-    sta FIRST_SPRITE, y ; store Y
-    ;---
-    inc TempPointY2
-    inc TempPointY
-    ldy TempPointY
-
-    lda (IntroSpritePtr), y
-
-    ldy TempPointY2
-    sta FIRST_SPRITE, y ; store frame
-    ;---
-    inc TempPointY2
-    inc TempPointY
-    ldy TempPointY
-
-    lda (IntroSpritePtr), y
-
-    ldy TempPointY2
-    sta FIRST_SPRITE, y ; store attribs
-    ;--
-    inc TempPointY2
-    inc TempPointY
-    ldy #0
-    lda (IntroSpriteCoordPtr), y ; x coord
-    ldy TempPointY
-    clc
-    adc (IntroSpritePtr), y
-
-    ldy TempPointY2
-    sta FIRST_SPRITE, y ; store X
-    ;--
-    inc TempPointY2
-    inc TempPointY
-    ldy TempPointY
-
-    rts
-
-;---------------------------------
-;TODO might be a duplicate
-HideIntroSprites:
-
-    lda #MAX_SPRITE_COUNT
-    cmp TempSpriteCount
-    bcc @done
-    sec
-    sbc TempSpriteCount
-
-    tax
-@hideSpritesLoop:
-    lda #$FE
-    sta FIRST_SPRITE, y
-    iny
-    iny
-    iny
-    iny
-
-    dex
-    bne @hideSpritesLoop
-@done:
-
-    rts
 ;---------------------------------
 ResetNameTableAdresses:
 
@@ -2061,131 +1894,7 @@ Logics:
 @exit:
 
     rts
-;-----------------------------
-IntroLogics:
 
-
-    inc IntroSpriteAnimFrame
-    lda IntroSpriteAnimFrame
-    cmp #2
-    bcc @go
-    lda #0
-    sta IntroSpriteAnimFrame
-@go:
-    lda IntroSceneIdx
-    cmp #INTRO_SCENE_MAX
-    bcs @exit
-    tax
-
-    ;do some scrolling
-    lda GlobalScroll
-    clc
-    adc intro_scroll_dir_x, x
-    sta GlobalScroll
-
-    lda GlobalScrollY
-    clc
-    adc intro_scroll_dir_y, x
-    sta GlobalScrollY
-
-
-    lda intro_scenes_duration, x
-    lsr
-    sta TempHp ; let's store half of the scene duration
-
-    lda IntroSceneIdx
-    asl
-    asl
-    tax
-
-    lda IntroTimer
-    cmp TempHp
-    bcs @cont
-
-    inx
-    inx
-
-@cont:
-    jsr MoveIntroSprites
-
-    dec IntroTimer
-    beq @increaseScene
-    jmp @exit
-
-@increaseScene:
-    inc IntroSceneIdx
-    lda IntroSceneIdx
-    cmp #INTRO_SCENE_MAX
-    bcs @StartGame
-
-    ldx IntroSceneIdx
-    lda intro_scenes_duration, x
-    sta IntroTimer
-    lda IntroSceneIdx
-    asl
-    tax
-    lda intro_sprite_pos_x, x
-    sta IntroSprite1X
-    lda intro_sprite_pos_y, x
-    sta IntroSprite1Y
-    inx
-    lda intro_sprite_pos_x, x
-    sta IntroSprite2X
-    lda intro_sprite_pos_y, x
-    sta IntroSprite2Y
-
-
-    lda #1
-    sta MustLoadIntro
-    sta MustLoadSomething
-    lda #0
-    sta MustLoadIntroChr
-    sta GlobalScroll
-
-    jmp @exit
-
-@StartGame:
-    jsr FadeOutToStartGame
-@exit:
-    rts
-
-;------------------------------
-FadeOutToStartGame:
-    lda #PALETTE_STATE_FADE_OUT
-    sta PaletteFadeAnimationState
-    lda #0
-    sta PaletteFadeTimer
-    sta FadeIdx
-    sta IsLocationRoutine
-    lda #FADE_DELAY_GENERIC
-    sta PaletteAnimDelay
-    rts
-
-
-;------------------------------
-MoveIntroSprites:
-    lda IntroSprite1X
-    clc
-    adc intro_sprite_dir_x, x
-    sta IntroSprite1X
-
-    lda IntroSprite1Y
-    clc
-    adc intro_sprite_dir_y, x
-    sta IntroSprite1Y
-
-    inx
-
-    lda IntroSprite2X
-    clc
-    adc intro_sprite_dir_x, x
-    sta IntroSprite2X
-
-    lda IntroSprite2Y
-    clc
-    adc intro_sprite_dir_y, x
-    sta IntroSprite2Y
-    rts
 
 ;------------------------------
 CheckEntryPoints:
@@ -2550,7 +2259,7 @@ RoutinesAfterFadeOut:
     sty oldbank
     ldy #3
     jsr bankswitch_y
-    jsr DoSleep
+    jsr DoSleep ; from bank 3
     ldy oldbank
     jsr bankswitch_y
     lda #0
@@ -3959,32 +3668,15 @@ LoadTitle:
     ldy #5
     jsr bankswitch_y
 
-    lda #<title_palette
-    sta pointer
-    lda #>title_palette
-    sta pointer + 1
-    lda #32
-    sta Temp
-    jsr LoadPalette
-
-
-
-    lda #STATE_TITLE
-    sta GameState
     lda #$00
     sta $2000
     sta $2001
 
-    lda #<title
-    sta pointer
-    lda #>title
-    sta pointer+1
     lda PPUCTRL
     and #%11111110
     sta PPUCTRL
-    lda #$20
-    sta NametableAddress
-    jsr LoadNametable
+    jsr LoadTitleData ; from bank 5
+
     lda #0
     sta MustLoadTitle
     sta MustLoadSomething
@@ -4024,32 +3716,7 @@ LoadGameOver:
     ldy #5
     bankswitch
 
-    lda #<game_over
-    sta pointer
-    lda #>game_over
-    sta pointer+1
-    lda PPUCTRL
-    and #%11111110
-    sta PPUCTRL
-    lda #$20
-    sta NametableAddress
-    jsr LoadNametable
-
-
-    jsr SetDaysInGameOver
-
-    lda #<game_over_palette
-    sta PalettePtr
-    lda #>game_over_palette
-    sta PalettePtr + 1
-
-    lda #PALETTE_STATE_FADE_IN
-    sta PaletteFadeAnimationState
-    lda #PALETTE_FADE_MAX_ITERATION
-    sta FadeIdx
-    lda #FADE_DELAY_GAME_OVER
-    sta PaletteAnimDelay
-
+    jsr LoadGameOverData ; from bank 5
 
     lda #0
     sta MustLoadGameOver
@@ -4057,29 +3724,6 @@ LoadGameOver:
 
 
 @exit:
-    rts
-;------------------------------------
-SetDaysInGameOver:
-    lda $2002
-    lda NametableAddress
-    clc
-    adc #3
-    sta $2006
-    lda #$11
-    sta $2006
-
-    ldy #0
-@daysLoop:
-    lda #CHARACTER_ZERO
-    clc 
-    adc Days, y
-    sta $2007
-    iny
-    cpy #3
-    bne @daysLoop
-
-
-
     rts
 
 ;-------------------------------------
@@ -4354,51 +3998,12 @@ LoadIntro:
     sta pointer + 1
     jsr CopyCHRTiles
 
- 
-
 @loadScene:
 
     ldy #5
     jsr bankswitch_y
 
-    ldy IntroSceneIdx
-
-    lda intro_scenes_low, y
-    sta pointer
-    lda intro_scenes_high, y
-    sta pointer+1
-    lda #$24
-    sta NametableAddress
-    jsr LoadNametable
-    ldy IntroSceneIdx
-    lda intro_scenes_low, y
-    sta pointer
-    lda intro_scenes_high, y
-    sta pointer+1
-
-    lda #$20
-    sta NametableAddress
-    jsr LoadNametable
-
-    jsr ClearPalette
-
-    lda #<intro_palette
-    sta PalettePtr
-    lda #>intro_palette
-    sta PalettePtr + 1
-
-    lda #PALETTE_STATE_FADE_IN
-    sta PaletteFadeAnimationState
-    lda #PALETTE_FADE_MAX_ITERATION
-    sta FadeIdx
-    lda #FADE_DELAY_GAME_OVER
-    sta PaletteAnimDelay
-
-
-    lda #0
-    sta MustLoadIntro
-    sta MustLoadSomething
-
+    jsr LoadIntroScene ; from bank 5
 @exit:
     rts
 ;-------------------------------------
@@ -4431,6 +4036,19 @@ StartGame:
 
     rts
 
+;------------------------------
+FadeOutToStartGame:
+    lda #PALETTE_STATE_FADE_OUT
+    sta PaletteFadeAnimationState
+    lda #0
+    sta PaletteFadeTimer
+    sta FadeIdx
+    sta IsLocationRoutine
+    lda #FADE_DELAY_GENERIC
+    sta PaletteAnimDelay
+    rts
+
+
 ;-------------------------------------
 CheckStartButton:
 
@@ -4450,38 +4068,14 @@ CheckStartButton:
 
     ;On title state------
 
-    ;jsr ClearPalette
-
     lda #1
     sta MustLoadSomething
     sta MustLoadIntro
     sta MustLoadIntroChr
     ldy #5
     jsr bankswitch_y
-    lda #0
-    sta IntroSceneIdx
-    sta GlobalScroll
-    asl
-    tax
-    lda intro_sprite_pos_x, x
-    sta IntroSprite1X
-    lda intro_sprite_pos_y, x
-    sta IntroSprite1Y
-    inx
-    lda intro_sprite_pos_x, x
-    sta IntroSprite2X
-    lda intro_sprite_pos_y, x
-    sta IntroSprite2Y
 
-    ldx IntroSceneIdx
-    lda intro_scenes_duration, x
-    sta IntroTimer
-    lda intro_scenes_delay, x
-    sta IntroDelay
-
-    lda #STATE_INTRO
-    sta GameState
-
+    jsr InitIntro ; from bank 5
 
     jmp @exit
 @someOtherState: ; not title
