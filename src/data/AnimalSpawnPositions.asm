e838af21f87c03b0b08f095c523fb39c7210b81f
jrs0ul <jrs0ul@gmail.com>
2023-09-06 16:55:02 +0300

    tweaked animal spawn positions

--- src/data/AnimalSpawnPositions.asm
+++ src/data/AnimalSpawnPositions.asm
@@ -22,34 +22,34 @@ location_spawns_high:
 spawnpoints_base_loc_1:
     .byte 208, 48
     .byte 208, 120
-    .byte 208, 48
-    .byte 208, 192
+    .byte 232, 144
+    .byte 224, 192
 
     .byte 120, 72
     .byte 168, 184
     .byte 48, 128
-    .byte 98, 160
+    .byte 98, 200
 
-    .byte 120, 72
-    .byte 168, 184
-    .byte 48, 128
-    .byte 98, 160
+    .byte 48, 144
+    .byte 136, 80
+    .byte 224, 120
+    .byte 184, 200
 
-    .byte 120, 72
-    .byte 168, 184
-    .byte 48, 128
-    .byte 98, 160
+    .byte 40, 88
+    .byte 200, 72
+    .byte 112, 160
+    .byte 216, 152
 
 spawnpoints_loc_2:
-    .byte 120, 72
-    .byte 168, 184
-    .byte 48, 128
-    .byte 98, 160
+    .byte 56, 136
+    .byte 176, 200
+    .byte 232, 56
+    .byte 160, 144
 
-    .byte 120, 72
-    .byte 168, 184
-    .byte 48, 128
-    .byte 98, 160
+    .byte 56, 56
+    .byte 32, 200
+    .byte 184, 192
+    .byte 216, 96
 
     .byte 0, 0
     .byte 0, 0
@@ -62,10 +62,10 @@ spawnpoints_loc_2:
     .byte 0, 0
 
 spawnpoints_loc_3:
-    .byte 120, 72
-    .byte 168, 184
-    .byte 48, 128
-    .byte 98, 160
+    .byte 64, 200
+    .byte 232, 72
+    .byte 232, 176
+    .byte 40, 56
 
     .byte 120, 72
     .byte 168, 184
