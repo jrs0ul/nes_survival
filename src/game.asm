91ed886a4d950f6f279f3fbd6f2f6d610fad51f9
jrs0ul <jrs0ul@gmail.com>
2024-01-02 15:49:05 +0200

    created massive player sprite data list, moving to the left and attack works allright, everything else does not

--- src/game.asm
+++ src/game.asm
@@ -322,63 +322,172 @@ npc_anim_row_sequence:
 
 .segment "ROM1";-------
 
-player_anim_row_sequence:
-   .byte 0
-   .byte 16
-   .byte 32
-   .byte 16
-   .byte 48
-
 
 player_sprites_not_flip:
-    ;left
+    ;LEFT
+    ;head
     .byte 0, 0,  %00000011, 0 ;1st sprite
     .byte 0, 1,  %00000011, 8 ;2nd
+    ;body
+    ;frame 0
     .byte 8, 16, %00000011, 0 ;3rd
     .byte 8, 17, %00000011, 8 ;4th
-
-    ;up
-    .byte 0, 0,  %00000011, 0 ;1st sprite
-    .byte 0, 1,  %00000011, 8 ;2nd
-    .byte 8, 16, %00000011, 0 ;3rd
-    .byte 8, 17, %00000011, 8 ;4th
-
-    ;down
-    .byte 0, 0,  %00000011, 0 ;1st sprite
-    .byte 0, 1,  %00000011, 8 ;2nd
-    .byte 8, 16, %00000011, 0 ;3rd
-    .byte 8, 17, %00000011, 8 ;4th
-
+    ;frame 1
+    .byte 8, 32, %00000011, 0 ;3rd
+    .byte 8, 33, %00000011, 8 ;4th
+    ;frame 2
+    .byte 8, 48, %00000011, 0 ;3rd
+    .byte 8, 49, %00000011, 8 ;4th
+    ;frame 3
+    .byte 8, 32, %00000011, 0 ;3rd
+    .byte 8, 33, %00000011, 8 ;4th
     ;attack
-    .byte 0, 0,  %00000011, 0 ;1st sprite
-    .byte 0, 1,  %00000011, 8 ;2nd
+    .byte 8, 64, %00000011, 0 ;3rd
+    .byte 8, 65, %00000011, 8 ;4th
+
+    .byte 0,0,0,0
+    .byte 0,0,0,0
+    .byte 0,0,0,0
+    .byte 0,0,0,0
+
+    ;---------------------------------
+    ;UP
+    ;head
+    .byte 0, 2,  %00000011, 0 ;1st sprite
+    .byte 0, 2,  %01000011, 8 ;2nd
+    ;body
+    ;frame 0
+    .byte 8, 18, %00000011, 0 ;3rd
+    .byte 8, 34, %01000011, 8 ;4th
+    ;frame 1
     .byte 8, 16, %00000011, 0 ;3rd
     .byte 8, 17, %00000011, 8 ;4th
+    ;frame 2
+    .byte 8, 16, %00000011, 0 ;3rd
+    .byte 8, 17, %00000011, 8 ;4th
+    ;frame 3
+    .byte 8, 16, %00000011, 0 ;3rd
+    .byte 8, 17, %00000011, 8 ;4th
+    ;attack
+    .byte 8, 66, %00000011, 0 ;3rd
+    .byte 8, 67, %00000011, 8 ;4th
 
+    .byte 0,0,0,0
+    .byte 0,0,0,0
+    .byte 0,0,0,0
+    .byte 0,0,0,0
+
+
+    ;---------------------------------
+    ;DOWN
+    .byte 0, 3,  %00000011, 0 ;1st sprite
+    .byte 0, 3,  %01000011, 8 ;2nd
+    ;body
+    ;frame 0
+    .byte 8, 19, %00000011, 0 ;3rd
+    .byte 8, 51, %01000011, 8 ;4th
+    ;frame 1
+    .byte 8, 19, %00000011, 0 ;3rd
+    .byte 8, 51, %01000011, 8 ;4th
+    ;frame 2
+    .byte 8, 19, %00000011, 0 ;3rd
+    .byte 8, 51, %01000011, 8 ;4th
+    ;frame 3
+    .byte 8, 19, %00000011, 0 ;3rd
+    .byte 8, 51, %01000011, 8 ;4th
+    ;attack
+    .byte 8, 68, %00000011, 0 ;3rd
+    .byte 8, 69, %00000011, 8 ;4th
+
+    .byte 0,0,0,0
+    .byte 0,0,0,0
+    .byte 0,0,0,0
+    .byte 0,0,0,0
+
+
+    
 player_sprites_flip:
-    ;right
+    ;RIGHT
+    ;head
     .byte 0, 1,  %01000011, 0 ;1st
     .byte 0, 0,  %01000011, 8 ;2nd
+    ;body
+    ;frame 0
     .byte 8, 17, %01000011, 0 ;3rd
     .byte 8, 16, %01000011, 8 ;4th
-
-    ;up
-    .byte 0, 0,  %00000011, 0 ;1st sprite
-    .byte 0, 1,  %00000011, 8 ;2nd
+    ;frame 1
     .byte 8, 16, %00000011, 0 ;3rd
     .byte 8, 17, %00000011, 8 ;4th
-
-    ;down
-    .byte 0, 0,  %00000011, 0 ;1st sprite
-    .byte 0, 1,  %00000011, 8 ;2nd
+    ;frame 2
     .byte 8, 16, %00000011, 0 ;3rd
     .byte 8, 17, %00000011, 8 ;4th
-
+    ;frame 3
+    .byte 8, 16, %00000011, 0 ;3rd
+    .byte 8, 17, %00000011, 8 ;4th
     ;attack
-    .byte 0, 0,  %00000011, 0 ;1st sprite
-    .byte 0, 1,  %00000011, 8 ;2nd
     .byte 8, 16, %00000011, 0 ;3rd
     .byte 8, 17, %00000011, 8 ;4th
+
+    .byte 0,0,0,0
+    .byte 0,0,0,0
+    .byte 0,0,0,0
+    .byte 0,0,0,0
+
+    ;----------------------------------
+    ;UP
+    ;head
+    .byte 0, 2,  %01000011, 8 ;1st sprite
+    .byte 0, 2,  %00000011, 0 ;2nd
+    ;body
+    ;frame 0
+    .byte 8, 34, %00000011, 0 ;3rd
+    .byte 8, 18, %01000011, 8 ;4th
+    ;frame 1
+    .byte 8, 16, %00000011, 0 ;3rd
+    .byte 8, 17, %00000011, 8 ;4th
+    ;frame 2
+    .byte 8, 16, %00000011, 0 ;3rd
+    .byte 8, 17, %00000011, 8 ;4th
+    ;frame 3
+    .byte 8, 16, %00000011, 0 ;3rd
+    .byte 8, 17, %00000011, 8 ;4th
+    ;attack
+    .byte 8, 16, %00000011, 0 ;3rd
+    .byte 8, 17, %00000011, 8 ;4th
+
+    .byte 0,0,0,0
+    .byte 0,0,0,0
+    .byte 0,0,0,0
+    .byte 0,0,0,0
+
+    ;---------------------------------
+    ;DOWN
+    ;head
+    .byte 0, 3,  %01000011, 8 ;1st sprite
+    .byte 0, 3,  %00000011, 0 ;2nd
+    ;body
+    ;frame 0
+    .byte 8, 51, %00000011, 0 ;3rd
+    .byte 8, 19, %01000011, 8 ;4th
+    ;frame 1
+    .byte 8, 16, %00000011, 0 ;3rd
+    .byte 8, 17, %00000011, 8 ;4th
+    ;frame 2
+    .byte 8, 16, %00000011, 0 ;3rd
+    .byte 8, 17, %00000011, 8 ;4th
+    ;frame 3
+    .byte 8, 16, %00000011, 0 ;3rd
+    .byte 8, 17, %00000011, 8 ;4th
+    ;attack
+    .byte 8, 16, %00000011, 0 ;3rd
+    .byte 8, 17, %00000011, 8 ;4th
+
+    .byte 0,0,0,0
+    .byte 0,0,0,0
+    .byte 0,0,0,0
+    .byte 0,0,0,0
+
+
 
 ;---
 
@@ -6440,11 +6549,16 @@ UpdateSprites:
 ;--- MAIN CHARACTER:
 
     lda PlayerFrame
-    asl  ; frame * 2
+    asl
+    asl
+    asl
+    asl ;frame is 16 lines
     sta TempFrame
 
-    ldy PlayerAnimationRowIndex
-    lda player_anim_row_sequence, y
+    lda PlayerAnimationRowIndex
+    asl ;2 lines of anim
+    asl
+    asl ;x4 to get bytes
     sta TempAnimIndex
 
     ldx #0
@@ -6452,32 +6566,42 @@ UpdateSprites:
 
 @maincharloop:
 
-    sty TempPlayerSpriteIdx
+    sty TempPlayerSpriteIdx ; current row in player sprite data
     tya
+    ;add sprite data offset for a frame
+    clc
+    adc TempFrame
+
+    ; multiply by 4 to get to single sprite data byte
     asl
     asl
+
     ldy PlayerFlip
     beq @notFlipped
+
     clc
-    adc #64 ;64 byte jump to player_sprites_flip
+    adc #192 ;192 byte jump to player_sprites_flip
 @notFlipped:
+
+    cpx #7
+    bcc @skipAnim ; first row of sprites are not animated
+
+    clc
+    adc TempAnimIndex
+
+@skipAnim:
     tay
     lda PlayerY
     clc
     adc player_sprites_not_flip, y
-    sta FIRST_SPRITE, x
+    sta FIRST_SPRITE, x ; y coordinate
     inx
     iny
-    lda TempFrame
+    lda #0
     clc
     adc player_sprites_not_flip, y
-    cpx #8
-    bcc @skipAnimation
-    ;add animation frames
-    clc
-    adc TempAnimIndex
-@skipAnimation:
-    sta FIRST_SPRITE, x
+
+    sta FIRST_SPRITE, x ; sprite frame
     inx
     iny
     lda player_sprites_not_flip, y
