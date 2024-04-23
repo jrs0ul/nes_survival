39f54fc84e61f11392fb027866d0d0b0d0910d01
kl <jrs0ul@gmail.com>
2024-04-23 15:30:56 +0300

    created a python script to crop 4 top lines from all the maps

---
+++ CropMaps.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+import sys
+assert sys.version_info[0] >= 3, "Python 3 required."
+
+map_files = [
+            "field_bg.asm",
+            "field_bg1.asm",
+            "field_bg2.asm",
+            "field_bg4.asm",
+            "LOC3_bg1.asm",
+            "LOC3_bg0.asm",
+            "house.asm",
+            "villager_hut.asm",
+            "villager2_hut.asm",
+            "grannys_hut.asm",
+            "alien_bossroom.asm",
+            "alien_base1.asm",
+            "alien_base2.asm",
+            "cave0.asm",
+            "cave1.asm",
+            "dark_cave0.asm",
+            "dark_cave1.asm",
+            "babloc1.asm",
+            "babloc2.asm",
+            "babloc3.asm",
+            "field2_bg.asm",
+            "field2_bg1.asm",
+            "crashsite0.asm",
+            "crashsite.asm",
+            "location_with_cave.asm",
+            "location_with_cave2.asm",
+            "secret_cave0.asm"
+            ]
+
+map_path = "src/data/maps/"
+
+def crop_maps():
+
+    lines = []
+
+    for fileName in map_files:
+        print("Cropping " + fileName)
+
+
+        try:
+            fileIN = open(map_path + fileName)
+            lines = fileIN.readlines()
+        except IOError:
+            print("oops!")
+            return
+
+        dataOut = ""
+
+        for i in range(len(lines)):
+            if i < 1 or i > 8 :
+                dataOut += lines[i]
+
+        nameArray = fileName.split('.')
+        print(nameArray)
+
+        fileOut = open(map_path + nameArray[0] + "_crop.asm", "wt").write(dataOut)
+
+
+if __name__ == "__main__":
+    crop_maps()
